f00ab8fde31d1467ca51c7880847e7f9b7703bd7
Arael Seher <a.seher.85@gmail.com>
2025-03-12 11:13:10 -0700

    fulfilled requests in readme

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"52\u00b0 Fahrenheit is {TempConverter.FahrenheitToCelsius(52f):N1}\u00b0 Celsius");
+            
+            Console.WriteLine($"47\u00b0 Celsius is {TempConverter.CelsiusToFahrenheit(47):N1}\u00b0 Fahrenheit");
         }
     }
 }
